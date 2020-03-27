0b938a26095d8e603b0b4c1218358f0bb1c27a54
Jonathan Rochelli <jonathan.rochelli@gmail.com>
2020-03-27 15:40:12 +0100

    FEAT : Send files

--- front/index.js
+++ front/index.js
@@ -1,7 +1,12 @@
 function sendUserName(){
 	var userName = $("#userName").val();
-	var url = "http://90.89.7.235:8081/getViewerCount?boxname=textBox&username="+userName
-	$.get(url, function( data ) {
-	  $("#textBox").text(data);
-	});
+var url = "http://90.89.7.235:8081/nbViewers?boxname=textBox&username="+userName
+				$.get(url, function( data ) {
+				  $("#textBox").text(data);
+				});
+}
+
+function fillCompareColumn(index) {
+	var userCompare = $("#userCompare").val();
+	$("#user" + index).text(userCompare);
 }
